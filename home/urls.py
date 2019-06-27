37bd9a4f7361cba8c33b54895d39cf9b1fccfd44
Moses <ogbopinamoses@gmail.com>
2019-06-27 10:42:20 +0100

    Displaying Categories on Home Page

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.HomePageView.as_view(), name='home'),
+    path('', views.home_page_view, name='home'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
+from category.models import Category
+
 # Create your views here.
 class HomePageView(TemplateView):
     template_name = 'index.html'
+
+def home_page_view(request):
+    categories = Category.objects.all()
+
+    context = {
+        'categories': categories,
+    }
+
+    return render(request, 'index.html', context)
